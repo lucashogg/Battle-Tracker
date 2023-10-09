89b49046fe791642f7e08c1698de99fbe5d7fa45
Lucas Hogg <lucashogg@gmail.com>
2023-10-09 16:01:42 -0400

    Update

--- app.py
+++ app.py
@@ -12,11 +12,30 @@ app = Flask(__name__)
 key = os.urandom(24)
 app.secret_key = key
 
+# Configure database (Per Flask docs)
+DATABASE = "battletracker.db"
 
-# Configure SQLite database
-con = sqlite3.connect("battletracker.db")
-con.row_factory = sqlite3.Row
-cur = con.cursor()
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
 
 
 @app.after_request
@@ -31,7 +50,7 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
+    """Show homepage"""
     return render_template("index.html")
 
 
@@ -53,8 +72,9 @@ def login():
             return render_template("login.html")
 
         # Query database for username
-        rows = cur.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        rows = query_db(
+            "SELECT * FROM users WHERE username = ?",
+            [request.form.get("username")],
         )
 
         # Ensure username exists and password is correct
@@ -85,39 +105,44 @@ def logout():
     return redirect("/")
 
 
-# @app.route("/register", methods=["GET", "POST"])
-# def register():
-#     """Register user"""
-#     if request.method == "POST":
-#         # Check if username blank
-#         if not request.form.get("username"):
-#             return render_template("register.html")
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "POST":
+        # Check if username blank
+        if not request.form.get("username"):
+            return render_template("register.html")
 
-#         # Check if password blank
-#         elif not request.form.get("password"):
-#             return render_template("register.html")
+        # Check if password blank
+        elif not request.form.get("password"):
+            return render_template("register.html")
 
-#         # Check if password/confirmation are same
-#         elif request.form.get("password") != request.form.get("confirmation"):
-#             return render_template("register.html")
+        # Check if password/confirmation are same
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return render_template("register.html")
 
-#         else:
-#             all_users = db.execute("SELECT username FROM users")
-#             for user in all_users:
-#                 if user["username"] == request.form.get("username"):
-#                     return render_template("register.html")
+        else:
+            for user in query_db("SELECT username FROM users"):
+                if user["username"] == request.form.get("username"):
+                    return render_template("register.html")
 
-#             # Insert username and password into DB
-#             db.execute(
-#                 "INSERT INTO users (username, hash) VALUES (?, ?)",
-#                 request.form.get("username"),
-#                 generate_password_hash(request.form.get("confirmation")),
-#             )
+            # Insert username and password into DB
+            cur = get_db().cursor()
+            cur.execute(
+                "INSERT INTO users (username, hash) VALUES (?, ?)",
+                (
+                    request.form.get("username"),
+                    generate_password_hash(request.form.get("confirmation")),
+                ),
+            )
+            get_db().commit()
+            get_db().close()
 
-#             return redirect("/")
+            return redirect("/")
 
-#     else:
-#         return render_template("register.html")
+    else:
+        return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run()

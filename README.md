f195a7e817757b595e4e339622e4b41b1b9dc127
Lucas Hogg <lucashogg@gmail.com>
2023-10-25 17:56:46 -0400

    First Version

---
+++ README.md
@@ -0,0 +1,60 @@
+# Battle Tracker
+
+> This is my final project for the course: CS50
+
+## Short Description
+
+Welcome to Battle Tracker!
+
+Battle Tracker is a web app designed to assist the GM of a Dungeons and Dragons table top campaign. Battle Tracker will store unique characters or monsters each containing a few basic stats while keeping track of initiative in battles.
+
+## Features
+
+This project consists of utilizng [Flask](https://flask.palletsprojects.com/en/2.3.x/) in a [Python](https://www.python.org/) environment. All data is managed using [SQLite3](https://docs.python.org/3/library/sqlite3.html).
+
+## How does this work?
+
+Battle Tracker allows users to register a unique account which stores all of their data in three sqlite tables within battletracker.db:
+
+- users
+  - Stores username and hashed password
+- characters
+  - Stores characters and related stats that the user inputs
+- current_battle
+  - Stores character information while being used in the current battle
+
+### Register
+
+Registering an account creates a new user, which must be unique, and stores a hashed password via Flask `werkzeug.security`'s `generate_password_hash` upon creation.
+
+### Login
+
+A user is required to log into the web app providing their user name and password. Thanks to `werkzeug.security`'s `check_password_hash` we are able to check if the user input matches the stored password.
+
+Upon a successful check, a Flask `session` is created with a randomly generated `app.secret_key` that saves a session cookie in the browser while the user is logged in.
+
+### Add
+
+The Add page allows users to add unique characters to their roster. Upon filling out the required fields, a character is created in the characters table and matched to the user's unique id. This allows for correct querying throughout the rest of the app.
+
+### Characters
+
+The Characters page allows users to view, edit, or delete a character from their roster. When a character is selected, the database is queryied using the user's unique id and then matching with name of the selected character.
+
+### Index (Home)
+
+The Index, or Home page, brings everything together. Here a user can select characters from their roster to participate in a battle. Once characters are selected added via the dropdown they will appear in the table below. The user can then "Roll For Initiative" by click the button. This generates a random number between 1 and 20 while adding each specific character's modififier to the total. The characters are then sorted in descending order, allowing the user to determine which character starts first and so forth.
+
+During battle a character might receive damage or healing. To account for this the user can input a negative (damage) or positive (healing) value under the Damage/Healing column. Once clicking Submit, the Current HP value will reflect the change. If the damage is subtracts greater than "0", the value will stay at "0."
+
+A character might receive a special condition during battle. To account for this the user can select character and condition, click Add, and the condition will appear under the Condition column. To remove, follow the same instructions, but click "Remove."
+
+Finally, to remove characters from battle, simply click the corresponding "X" button for a character and it will remove it from the table.
+
+### Logout
+
+If a user is logged out the `session` is cleared.
+
+## Video Demo
+
+[Battle Tracker](https://youtu.be/LeWseRsh2qk)
--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 
 import sqlite3
-from flask import Flask, redirect, render_template, request, session, g
+import random
+from flask import Flask, redirect, render_template, request, session, g, flash
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import login_required
@@ -47,11 +48,237 @@ def after_request(response):
     return response
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
     """Show homepage"""
-    return render_template("index.html")
+
+    # Grab currrent battle characters
+    current_battle = query_db(
+        "SELECT * FROM current_battle WHERE user_id = ? ORDER BY initiative DESC",
+        [session["user_id"]],
+    )
+    # Grab currrent users characters
+    chars = query_db(
+        "SELECT * FROM characters WHERE user_id = ?",
+        [session["user_id"]],
+    )
+
+    cond = [
+        "BLINDED",
+        "CHARMED",
+        "DEAFENED",
+        "EXHAUSTION",
+        "FRIGHTENED",
+        "GRAPPLED",
+        "INCAPACITATED",
+        "INVISIBLE",
+        "PARALYZED",
+        "PETRIFIED",
+        "POISONED",
+        "PRONE",
+        "RESTRAINED",
+        "STUNNED",
+        "UNCOUNSCIOUS",
+    ]
+
+    # Select character from dropdown
+    if request.method == "POST":
+        char = query_db(
+            "SELECT * FROM characters WHERE user_id = ? AND name = ?",
+            [session["user_id"], request.form.get("char-select")],
+        )
+
+        battle_char = query_db(
+            "SELECT * FROM current_battle WHERE user_id = ? AND name = ?",
+            [session["user_id"], request.form.get("char-select")],
+        )
+
+        if not battle_char:
+            for data in char:
+                cur = get_db().cursor()
+                cur.execute(
+                    "INSERT INTO current_battle (name, ac, max_hp, current_hp, modifier, user_id) VALUES (?, ?, ?, ?, ?, ?)",
+                    (
+                        data["name"],
+                        data["ac"],
+                        data["max_hp"],
+                        data["current_hp"],
+                        data["modifier"],
+                        session["user_id"],
+                    ),
+                )
+                get_db().commit()
+                get_db().close()
+
+            return redirect("/")
+
+        else:
+            flash("Character aleady in battle session")
+
+    return render_template(
+        "index.html", current_battle=current_battle, chars=chars, cond=cond
+    )
+
+
+@app.route("/battle", methods=["GET", "POST"])
+@login_required
+def battle():
+    """Battle actions"""
+    if request.method == "POST":
+        # Delete a character from battle
+        if request.form["battle"] == "deletechar":
+            cur = get_db().cursor()
+            cur.execute(
+                "DELETE FROM current_battle WHERE user_id = ? AND name = ?",
+                [session["user_id"], request.form.get("deletechar")],
+            )
+            get_db().commit()
+            get_db().close()
+
+        # Update current HP
+        elif request.form["battle"] == "damage":
+            damage = request.form.get("damage")
+            name = request.form.get("currenthp")
+
+            current_hp = query_db(
+                "SELECT current_hp FROM current_battle WHERE user_id = ? AND name = ?",
+                [session["user_id"], name],
+            )
+
+            max_hp = query_db(
+                "SELECT max_hp FROM current_battle WHERE user_id = ? AND name = ?",
+                [session["user_id"], name],
+            )
+
+            for data in current_hp:
+                new_hp = data["current_hp"] + int(damage)
+                if new_hp < 0:
+                    new_hp = 0
+                elif new_hp > max_hp[0]["max_hp"]:
+                    new_hp = max_hp[0]["max_hp"]
+
+                cur = get_db().cursor()
+                cur.execute(
+                    "UPDATE current_battle SET current_hp = ? WHERE user_id = ? AND name = ?",
+                    [
+                        new_hp,
+                        session["user_id"],
+                        name,
+                    ],
+                )
+                get_db().commit()
+                get_db().close()
+
+        return redirect("/")
+
+    return redirect("/")
+
+
+@app.route("/initiative", methods=["POST"])
+@login_required
+def initiative():
+    modifier = query_db(
+        "SELECT modifier, name FROM current_battle WHERE user_id = ?",
+        [session["user_id"]],
+    )
+
+    # chars = []
+    # initiative = int(data["modifier"]) + random.randint(1, 20)
+
+    for data in modifier:
+        initiative = int(data["modifier"]) + random.randint(1, 20)
+        # dict = {"name": data["name"], "init": initiative}
+        # chars.append(dict)
+        name = data["name"]
+
+        cur = get_db().cursor()
+        cur.execute(
+            "UPDATE current_battle SET initiative = ? WHERE user_id = ? AND name = ?",
+            [
+                initiative,
+                session["user_id"],
+                name,
+            ],
+        )
+        get_db().commit()
+    get_db().close()
+
+    return redirect("/")
+
+
+@app.route("/currenthp", methods=["POST"])
+@login_required
+def currenthp():
+    """Update current hp"""
+    damage = request.form.get("damage")
+    name = request.form.get("currenthp")
+
+    current_hp = query_db(
+        "SELECT current_hp FROM current_battle WHERE user_id = ? AND name = ?",
+        [session["user_id"], name],
+    )
+
+    for data in current_hp:
+        new_hp = data["current_hp"] + int(damage)
+
+        cur = get_db().cursor()
+        cur.execute(
+            "UPDATE current_battle SET current_hp = ? WHERE user_id = ? AND name = ?",
+            [
+                new_hp,
+                session["user_id"],
+                name,
+            ],
+        )
+        get_db().commit()
+        get_db().close()
+
+    return redirect("/")
+
+
+@app.route("/condition", methods=["POST"])
+@login_required
+def condition():
+    """Update character condition"""
+    name = request.form.get("char-select")
+    condition = request.form.get("condition")
+
+    cond = query_db(
+        "SELECT condition FROM current_battle WHERE user_id = ? AND name = ?",
+        [session["user_id"], name],
+    )
+
+    for data in cond:
+        if request.form["cond"] == "add":
+            if data["condition"] != condition:
+                cur = get_db().cursor()
+                cur.execute(
+                    "UPDATE current_battle SET condition = ? WHERE user_id = ? AND name = ?",
+                    [
+                        condition,
+                        session["user_id"],
+                        name,
+                    ],
+                )
+                get_db().commit()
+                get_db().close()
+
+        elif request.form["cond"] == "remove":
+            if data["condition"] == condition:
+                cur = get_db().cursor()
+                cur.execute(
+                    "UPDATE current_battle SET condition = ? WHERE user_id = ? AND name = ?",
+                    [
+                        None,
+                        session["user_id"],
+                        name,
+                    ],
+                )
+                get_db().commit()
+                get_db().close()
+
+    return redirect("/")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -62,36 +289,41 @@ def login():
     session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
+    error = None
     if request.method == "POST":
         # Ensure username was submitted
         if not request.form.get("username"):
-            return render_template("login.html")
+            error = "Invalid credentials"
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return render_template("login.html")
+            error = "Invalid credentials"
 
-        # Query database for username
-        rows = query_db(
-            "SELECT * FROM users WHERE username = ?",
-            [request.form.get("username")],
-        )
+        else:
+            # Query database for username
+            user = query_db(
+                "SELECT * FROM users WHERE username = ?",
+                [request.form.get("username")],
+                one=True,
+            )
+            # Ensure username exists and password is correct
+            if user is None or not check_password_hash(
+                user["hash"], request.form.get("password")
+            ):
+                error = "Invalid credentials"
 
-        # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows[0]["hash"], request.form.get("password")
-        ):
-            return render_template("login.html")
+            else:
+                # Remember which user has logged in
+                session["user_id"] = user["id"]
 
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+                # Redirect user to home page
+                return redirect("/")
 
-        # Redirect user to home page
-        return redirect("/")
+        return render_template("login.html", error=error)
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        return render_template("login.html", error=error)
 
 
 @app.route("/logout")
@@ -108,40 +340,243 @@ def logout():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
+    error = None
     if request.method == "POST":
         # Check if username blank
         if not request.form.get("username"):
-            return render_template("register.html")
+            error = "Invalid credentials"
 
         # Check if password blank
         elif not request.form.get("password"):
-            return render_template("register.html")
+            error = "Invalid credentials"
 
         # Check if password/confirmation are same
         elif request.form.get("password") != request.form.get("confirmation"):
-            return render_template("register.html")
+            error = "Invalid credentials"
 
         else:
-            for user in query_db("SELECT username FROM users"):
-                if user["username"] == request.form.get("username"):
-                    return render_template("register.html")
-
-            # Insert username and password into DB
-            cur = get_db().cursor()
-            cur.execute(
-                "INSERT INTO users (username, hash) VALUES (?, ?)",
-                (
-                    request.form.get("username"),
-                    generate_password_hash(request.form.get("confirmation")),
-                ),
+            # Check if user already exists
+            user = query_db(
+                "SELECT username FROM users WHERE username = ?",
+                [request.form.get("username")],
+                one=True,
             )
-            get_db().commit()
-            get_db().close()
 
-            return redirect("/")
+            # If user doesn't exister, ceate new user
+            if user is None:
+                # Insert username and password into DB
+                cur = get_db().cursor()
+                cur.execute(
+                    "INSERT INTO users (username, hash) VALUES (?, ?)",
+                    (
+                        request.form.get("username"),
+                        generate_password_hash(request.form.get("confirmation")),
+                    ),
+                )
+                get_db().commit()
+                get_db().close()
+
+                return redirect("/")
+
+            # If user exists, print error
+            else:
+                error = "Select a different user name"
+
+        return render_template("register.html", error=error)
 
     else:
         return render_template("register.html")
+
+
+@app.route("/characters", methods=["GET", "POST"])
+@login_required
+def characters():
+    """Character page"""
+
+    # Grab currrent users characters
+    chars = query_db(
+        "SELECT * FROM characters WHERE user_id = ?",
+        [session["user_id"]],
+    )
+
+    # Select character from dropdown
+    if request.method == "POST":
+        char = query_db(
+            "SELECT * FROM characters WHERE user_id = ? AND name = ?",
+            [session["user_id"], request.form.get("char-select")],
+        )
+
+        return render_template("characters.html", char=char, chars=chars)
+
+    return render_template("characters.html", chars=chars)
+
+
+@app.route("/edit", methods=["POST"])
+@login_required
+def edit():
+    """Character edit function"""
+
+    # Get values from inputs
+    charname = request.form.get("charname")
+    armorclass = request.form.get("armorclass")
+    maxhp = request.form.get("maxhp")
+    currenthp = request.form.get("currenthp")
+    modifier = request.form.get("modifier")
+
+    # Make sure at least one field (and positive values) is entered
+    if (
+        not charname
+        and (not armorclass or int(armorclass) < 1)
+        and (not maxhp or int(maxhp) < 1)
+        and (not currenthp or int(currenthp) < 0)
+        and (not modifier or int(modifier) < 0)
+    ):
+        flash("At least one field must be filled out or enter only positve values")
+
+    # Update character in database
+    else:
+        if charname:
+            cur = get_db().cursor()
+            cur.execute(
+                "UPDATE characters SET name = ? WHERE user_id = ? and name = ?",
+                (
+                    charname,
+                    session["user_id"],
+                    request.form.get("edit"),
+                ),
+            )
+
+        if armorclass:
+            cur = get_db().cursor()
+            cur.execute(
+                "UPDATE characters SET ac = ? WHERE user_id = ? and name = ?",
+                (
+                    armorclass,
+                    session["user_id"],
+                    request.form.get("edit"),
+                ),
+            )
+
+        if maxhp:
+            cur = get_db().cursor()
+            cur.execute(
+                "UPDATE characters SET max_hp = ? WHERE user_id = ? and name = ?",
+                (
+                    maxhp,
+                    session["user_id"],
+                    request.form.get("edit"),
+                ),
+            )
+
+        if currenthp:
+            cur = get_db().cursor()
+            cur.execute(
+                "UPDATE characters SET current_hp = ? WHERE user_id = ? and name = ?",
+                (
+                    currenthp,
+                    session["user_id"],
+                    request.form.get("edit"),
+                ),
+            )
+
+        if modifier:
+            cur = get_db().cursor()
+            cur.execute(
+                "UPDATE characters SET modifier = ? WHERE user_id = ? and name = ?",
+                (
+                    modifier,
+                    session["user_id"],
+                    request.form.get("edit"),
+                ),
+            )
+
+        get_db().commit()
+        get_db().close()
+
+        flash("Character Successfully Edited")
+
+    return redirect("/characters")
+
+
+@app.route("/add", methods=["GET", "POST"])
+@login_required
+def add():
+    """Add character page"""
+
+    if request.method == "POST":
+        # Check if fields are left blank
+        if not request.form.get("charname"):
+            flash("Must enter character name")
+
+        elif (
+            not request.form.get("armorclass")
+            or int(request.form.get("armorclass")) < 1
+        ):
+            flash("Must enter armor class")
+
+        elif not request.form.get("maxhp") or int(request.form.get("maxhp")) < 1:
+            flash("Must enter max hp")
+
+        elif (
+            not request.form.get("currenthp") or int(request.form.get("currenthp")) < 0
+        ):
+            flash("Must enter current hp")
+
+        elif not request.form.get("modifier") or int(request.form.get("modifier")) < 0:
+            flash("Must enter modifier")
+
+        else:
+            # Check if character name exists
+            char = query_db(
+                "SELECT name FROM characters WHERE user_id = ? AND name = ?",
+                [session["user_id"], request.form.get("charname")],
+                one=True,
+            )
+
+            if char is None:
+                # If name is unique, add to db
+                cur = get_db().cursor()
+                cur.execute(
+                    "INSERT INTO characters (name, ac, max_hp, current_hp, modifier, user_id) VALUES (?, ?, ?, ?, ?, ?)",
+                    (
+                        request.form.get("charname"),
+                        request.form.get("armorclass"),
+                        request.form.get("maxhp"),
+                        request.form.get("currenthp"),
+                        request.form.get("modifier"),
+                        session["user_id"],
+                    ),
+                )
+                get_db().commit()
+                get_db().close()
+
+                flash("Character Successfully Added")
+
+                return render_template("add.html")
+
+            else:
+                flash("Character name already taken")
+
+        return render_template("add.html")
+
+    return render_template("add.html")
+
+
+@app.route("/removechar", methods=["POST"])
+@login_required
+def removechar():
+    """Delete character from database"""
+    cur = get_db().cursor()
+    cur.execute(
+        "DELETE FROM characters WHERE user_id = ? AND name = ?",
+        [session["user_id"], request.form.get("remove-char")],
+    )
+    get_db().commit()
+    get_db().close()
+
+    flash("Character Successfully Deleted")
+
+    return redirect("/characters")
 
 
 if __name__ == "__main__":
